f2ff66cd59f136692d7cc250a48a6b3b8b2123e8
Kode00e <zaimosheng@gmail.com>
2026-06-24 22:18:46 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+var CACHE='encrypsicode-v1';
+var URLS=['/','/index.html','/manifest.json'];
+
+self.addEventListener('install',function(e){
+  e.waitUntil(
+    caches.open(CACHE).then(function(c){
+      return c.addAll(URLS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate',function(e){
+  e.waitUntil(
+    caches.keys().then(function(ks){
+      return Promise.all(
+        ks.filter(function(k){return k!==CACHE}).map(function(k){return caches.delete(k)})
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch',function(e){
+  e.respondWith(
+    caches.match(e.request).then(function(r){
+      return r||fetch(e.request);
+    })
+  );
+});
